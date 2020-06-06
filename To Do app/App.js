0fd2d3ad9bb971907ba6fd90a0da3858059f162c
Rinava <asdasd5056@gmail.com>
2020-06-05 21:48:58 -0300

    Created basic structure

--- To Do app/App.js
+++ To Do app/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, CheckBox, ScrollView, Button, TextInput } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends React.Component  {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Button
+          title="See Completed"></Button>
+        </View>
+        <TextInput placeholder="Give me more tasks :)"></TextInput>
+        <Button 
+        title="Add"></Button>
+        <Text>To Do:</Text>
+        <ScrollView>
+          <Text>Example 1</Text>
+          <CheckBox></CheckBox>
+        </ScrollView>
+      </View>
+
+    );
+  }
 }
 
 const styles = StyleSheet.create({
